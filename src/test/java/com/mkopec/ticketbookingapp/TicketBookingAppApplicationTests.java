8d0c601a6ca1d1e5a11e301d5896f8e2f987ae18
= <=>
2019-06-29 08:58:01 +0200

    Initial commit

---
+++ src/test/java/com/mkopec/ticketbookingapp/TicketBookingAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mkopec.ticketbookingapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TicketBookingAppApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
