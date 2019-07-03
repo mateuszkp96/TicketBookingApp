18119597cc53336c5c9f65a692c434e6cdad87c0
mateuszkp96 <mateuszkp96@gmail.com>
2019-07-03 15:32:21 +0200

    Validation in save method of ReservationService added

--- src/main/java/com/mkopec/ticketbookingapp/service/ReservationService.java
+++ src/main/java/com/mkopec/ticketbookingapp/service/ReservationService.java
@@ -6,15 +6,29 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.math.BigDecimal;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 @Service
 @RequiredArgsConstructor
 public class ReservationService {
     private final ReservationRepository repository;
+    private final ReservationValidationService validationService;
+    private final TicketTypeService ticketTypeService;
 
     @Transactional
     public Reservation saveReservation(Reservation reservation) {
+        
+        validationService.validate(reservation);
+        reservation.setExpirationTime(validationService.getExpirationTime(reservation));
+
+        List<Long> ticketTypesIDs = reservation.getTickets().stream()
+                .map(ticket -> ticket.getTicketType().getId()).collect(toList());
+        BigDecimal payment = ticketTypeService.getPaymentForTickets(ticketTypesIDs);
+        reservation.setPayment(payment);
+
         return repository.save(reservation);
     }
-
 }
